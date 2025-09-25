dee6685407ca20935797477c28fea4f0ee2edec8
Tharani150408 <tharani150408@gmail.com>
2025-09-25 09:03:13 +0530

    first commit

---
+++ Task 1.js
@@ -0,0 +1,16 @@
+let num="74";
+let a=Number(num);
+
+switch(true){
+    case num>=90 && num<=100:
+    console.log("Grade: A");
+    break;
+    case num>=75 && num<=89:
+        console.log("Grade: B");
+        break;
+        case num>=50 && num<=74:
+            console.log("Grade: C");
+            break;
+        default:
+            console.log("Fail");
+}
